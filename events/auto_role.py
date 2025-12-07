eea7af33db027aa8f9e303f898a256c6860dda7c
kajongwe <kajongwe@fastmail.com>
2025-12-06 21:26:03 -0600

    feat: reimplement auto-roles
    now supports more than one role if ever necessary

--- events/auto_role.py
+++ events/auto_role.py
@@ -1,21 +1,24 @@
-from discord.ext import commands
-import discord
+from discord.ext.commands import Bot, Cog
+from discord import Member, Object
 from config.config import config
 from logger.logger import logger
 
 
-class auto_role(commands.Cog):
-    def __init__(self, bot):
-        self.bot: commands.Bot = bot
+class AutoRole(Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
         self.logger = logger.instance()
         self.config = config.read_config()
 
-    @commands.Cog.listener()
-    async def on_member_join(self, member: discord.Member):
-        roles = await member.guild.fetch_roles()
-        new_role =  list(filter(lambda x: x.name == self.config["defaultrole"], roles))[0]
-        await member.add_roles(new_role)
-        self.logger.log(f'added role {new_role} to {member.name}')
+    @Cog.listener()
+    async def on_member_join(self, member: Member):
+        role_ids: map[Object] = map(
+            lambda x: Object(x.id),
+            filter(lambda x: x.id in self.config["defaultRoles"], member.guild.roles),
+        )
 
-async def setup(bot):
-    await bot.add_cog(auto_role(bot))
+        # TODO: Log action
+        await member.add_roles(*role_ids)
+
+async def setup(bot: Bot):
+    await bot.add_cog(AutoRole(bot))
