d7828da46c995218397c65d31d062fe702481d7d
kajongwe <kajongwe@fastmail.com>
2025-12-29 15:42:08 -0600

    chore: clean up cringe event
    TODO: embed is now (almost) generic, it can be reused in ;;wood pretty easily
    TODO: do we want to special case anything more than twitter
    TODO: replace uses of parse_message_into_embed with the rewrite when appropriate
    
    TODO: have more of the project be guild-aware

--- base/utils.py
+++ base/utils.py
@@ -1,15 +1,130 @@
+import json
+import re
+import requests
 from datetime import datetime, timezone
 from discord import Embed, Message
-import re
+from urllib.parse import urlparse
+
 from discord.ext.commands.bot import logging
-import requests
 
 URL_REGEX = r"https?://(?:www\.)?(?:[a-zA-Z0-9@:%._+~#=]{1,256}\.)(?:[a-zA-Z0-9()]{1,6})\b(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)"
 
-TWITTER_REGEX = r"https?:\/\/(?:www\.)?(twitter|x|fxtwitter|vxtwitter|fixupx|girlcockx)\.com"
+TWITTER_REGEX = (
+    r"https?:\/\/(?:www\.)?(twitter|x|fxtwitter|vxtwitter|fixupx|girlcockx)\.com"
+)
+
+COLOUR_SAPWOOD = 0xE8B693
+DUMMY_URL = "https://example.com"
+
+url_regex = re.compile(URL_REGEX)
 
 
-def parse_message_into_embed(message: Message, color: int, author: tuple[str,str], footer: str, content_override: bool = False, extra_fields: list[tuple[str,str,bool]] = []) -> list[Embed]:
+def cringe_embed(message: Message) -> list[Embed]:
+    embeds: list[Embed] = []
+
+    main = (
+        Embed(
+            color=COLOUR_SAPWOOD,
+            url=DUMMY_URL,
+            timestamp=datetime.now(tz=timezone.utc),
+        )
+        .set_author(
+            name=f"{message.author.name} posted cringe",
+            icon_url=message.author.display_avatar.url,
+        )
+        .set_footer(text=f"ID: {message.id}")
+    )
+
+    if message.content != "":
+        main.add_field(name="Message", value=message.content)
+
+    main.add_field(name="Link", value=message.jump_url)
+
+    links: list[str] = url_regex.findall(message.content)
+    attachments: list[str] = list(map(lambda x: x.url, message.attachments))
+
+    # eg. a tweet with multiple images, only one can be the main image
+    extra_img_urls = []
+
+    match (
+        len(links) > 0,
+        len(attachments) > 0,
+    ):
+        case (True, False):
+            img_urls = handle_link(links[0])
+            links = links[1::]  # remove the link that was just handled
+
+            main.set_image(url=img_urls[0])
+            extra_img_urls = img_urls[1::]
+
+        case (False, True):
+            main.set_image(url=attachments[0])
+            attachments = attachments[1::]
+
+        case (True, True):
+            main.set_image(url=attachments[0])
+            attachments = attachments[1::]
+        case _:
+            pass
+
+    embeds.append(main)
+
+    for url in extra_img_urls:
+        embeds.append(Embed(url=DUMMY_URL).set_image(url=url))
+
+    for link in links:
+        img_urls = handle_link(link)
+
+        for url in img_urls:
+            embeds.append(Embed(url=DUMMY_URL).set_image(url=url))
+
+    for url in attachments:
+        embeds.append(Embed(url=DUMMY_URL).set_image(url=url))
+
+    return embeds
+
+
+def handle_link(url: str) -> list[str]:
+    logger = logging.getLogger("utils.cringe.handle_link")
+    result = urlparse(url)
+
+    match result.netloc:
+        # TODO: how many more of these things do dsd members use...
+        case (
+            "x.com"
+            | "twitter.com"
+            | "fixupx.com"
+            | "fixvx.com"
+            | "cunnyx.com"
+            | "vxtwitter.com"
+            | "fxtwitter.com"
+            | "hotyurisex.com"
+            | "girlcockx.com"
+        ):
+            api = result._replace(netloc="api.fxtwitter.com")
+
+            r = requests.get(api.geturl())
+            if r.status_code != 200:
+                logger.error(f"{r.status_code} from {api.geturl()}")
+
+            obj = json.loads(r.text)
+            urls: list[str] = list(
+                map(lambda x: x["url"], obj["tweet"]["media"]["photos"])
+            )
+
+            return urls
+        case _:
+            return [url]
+
+
+def parse_message_into_embed(
+    message: Message,
+    color: int,
+    author: tuple[str, str],
+    footer: str,
+    content_override: bool = False,
+    extra_fields: list[tuple[str, str, bool]] = [],
+) -> list[Embed]:
     """
     receives a message
     creates a main embed with color, author[name, icon_url], footer
@@ -26,25 +141,26 @@ def parse_message_into_embed(message: Message, color: int, author: tuple[str,str
 
     NEEDS TO BE TESTED !!!
     """
-    
+
     main_embed = Embed(
-            color=color, 
-            url="https://example.com", # will this also work in python?
-            timestamp=datetime.now(tz=timezone.utc)
-            )
+        color=color,
+        url="https://example.com",  # will this also work in python?
+        timestamp=datetime.now(tz=timezone.utc),
+    )
     main_embed.set_author(name=author[0], icon_url=author[1])
     main_embed.set_footer(text=footer)
 
-    if message.content and not content_overried:
+    if message.content and not content_override:
         main_embed.add_field(name="Message", value=message.content, inline=False)
 
     for field in extra_fields:
         main_embed.add_field(name=field[0], value=field[1], inline=field[2])
 
-    main_embed.add_field(name="Link", value=message.jump_url, inline=False) # TODO: remove this and force outsiders to put it in extra_fileds
+    main_embed.add_field(
+        name="Link", value=message.jump_url, inline=False
+    )  # TODO: remove this and force outsiders to put it in extra_fileds
 
     hyperlinks = re.findall(URL_REGEX, message.content)
-
 
     if message.attachments:
         main_embed.set_image(url=message.attachments[0].url)
@@ -54,9 +170,7 @@ def parse_message_into_embed(message: Message, color: int, author: tuple[str,str
     media_urls = fetch_media_url(hyperlinks)
 
     for url in media_urls:
-        _embed = Embed(
-            url="https://example.com"
-            )
+        _embed = Embed(url="https://example.com")
         _embed.set_image(url=url)
         embeds.append(_embed)
 
@@ -82,4 +196,3 @@ def fetch_media_url(hyperlinks: list[str]) -> list[str]:
             pass
 
     return photo_urls
-
--- events/cringe_event.py
+++ events/cringe_event.py
@@ -1,64 +1,109 @@
-from datetime import timedelta, datetime, timezone
-import logging
 import traceback
+import logging
+
+from datetime import timedelta, datetime, timezone
 from discord.ext.commands import Bot, Cog
-from discord import RawReactionActionEvent
+from discord import RawReactionActionEvent, TextChannel, Member
 from base.config import Config
+from base.database import db
 from dao.cringe_dao import CringeDAO
-from models.cringe import Cringe
-from base.utils import parse_message_into_embed
+from models.cringe import CringeMessage
+from base.utils import cringe_embed
+
+# TODO: DRY. Make ;;wood and ;;cringe reuse a decent amt. of code
 
 
 class CringeEvent(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.logger = logging.getLogger(__name__)
         self.config = Config.read_config()
-        self.dao = CringeDAO()
+        self.dao = CringeDAO(db)
 
+    @property
+    def logger(self):
+        return logging.getLogger(__name__)
+
+    # on_reaction_add doesnt get invoked when reacting to older messages before the bot was started
     @Cog.listener()
-    async def on_raw_reaction_add(self, payload: RawReactionActionEvent): # on_reaction_add doesnt get invoked when reacting to older messages before the bot was started
-        """
-        TODO: media support
-        keeping the try/catch for debugging until its finished
-        """
-        try:
-            cringe_emoji = self.config["emoji"]["cringe"]
-            reaction_emoji = str(payload.emoji)
-            if reaction_emoji != cringe_emoji:
-                return
-            
-            channel = await self.bot.fetch_channel(payload.channel_id)
-            message = await channel.fetch_message(payload.message_id)
-            
-            if message.author.id == payload.member.id:
-                await message.remove_reaction(reaction_emoji, message.author) # same user can't react
-                return
-            
-            
-            if (datetime.now(tz=timezone.utc) - message.created_at).seconds > self.config["defaultCringeConfig"]["expireTime"] * 60: # created_at returns time in utc
-                return 
-            
-            already_cringed = self.dao.get_one(Cringe.from_message(message))
-            if already_cringed:
-                return
-            
-            cringe_count = list(filter(lambda x: str(x.emoji) == cringe_emoji, message.reactions))[0].count
-            
-            if cringe_count >= self.config["Cringe"]["threshold"]:
-                await message.author.timeout(timedelta(minutes=cringe_config["timeoutTime"]))
-                self.dao.add(Cringe.from_message(message))
-                self.logger.info(f"{message.author} has been timed out for {self.config['defaultCringeConfig']['timeoutTime']} minutes")
+    async def on_raw_reaction_add(self, payload: RawReactionActionEvent):
+        target_emoji: str = self.config["emoji"]["cringe"]
+        reacted_emoji = str(payload.emoji)
 
-                _embeds = parse_message_into_embed(message, 0xe8b693, (f"{message.author.name} posted cringe", message.author.display_avatar.url), f"ID: {message.id}")
+        if reacted_emoji != target_emoji:
+            return
 
-                await channel.send(embeds=_embeds)
-        except Exception as e:
-            self.logger.error(traceback.format_exc())
-            
-            
+        guild = await guild_name(self.bot, payload)
 
+        msg_ch = await self.bot.fetch_channel(payload.channel_id)
+        if not isinstance(msg_ch, TextChannel):
+            return
+
+        log_ch = await self.bot.fetch_channel(self.config["cringe"]["channelId"])
+        if not isinstance(log_ch, TextChannel):
+            self.logger.error(f"{guild}: config.cringe.channelId should be TextChannel")
+            return
+
+        message = await msg_ch.fetch_message(payload.message_id)
+        if not isinstance(message.author, Member):
+            return
+
+        if payload.member is None:
+            self.logger.error("invariant: expected payload.member != None")
+            return
+
+        if message.author.id == payload.member.id:
+            await message.remove_reaction(reacted_emoji, message.author)
+            self.logger.info(f"{guild}: {message.author.name} tried muting themselves")
+            return
+
+        elapsed = datetime.now(tz=timezone.utc) - message.created_at
+        expireTime = self.config["cringe"]["expireTime"] * 60
+
+        if elapsed.seconds >= expireTime:  # created_at returns time in utc
+            self.logger.info(
+                f"{guild}: reaction to {message.author.name}'s message outside window: {elapsed.seconds}s elapsed'"
+            )
+            return
+
+        self.logger.info(
+            f"{guild}: {payload.member.name} thinks {message.author.name} is cringe"
+        )
+
+        already_cringed = self.dao.get_one(CringeMessage.from_message(message))
+        if already_cringed:
+            return
+
+        cringe_count = list(
+            filter(lambda x: str(x.emoji) == target_emoji, message.reactions)
+        )[0].count
+
+        if cringe_count >= self.config["cringe"]["threshold"]:
+            duration = timedelta(minutes=self.config["cringe"]["timeoutTime"])
+
+            try:
+                await message.author.timeout(duration)
+            except Exception as _:
+                self.logger.error(traceback.format_exc())
+
+            self.dao.add(CringeMessage.from_message(message))
+            self.logger.info(
+                f"{guild}: timed out {message.author} for {duration.seconds}s"
+            )
+
+            await message.reply(
+                f"{message.author.name.upper()} WAS MUTED FOR THIS POST",
+                mention_author=True,
+            )
+
+            await log_ch.send(embeds=cringe_embed(message))
 
 
 async def setup(bot: Bot):
     await bot.add_cog(CringeEvent(bot))
+
+
+async def guild_name(bot: Bot, payload: RawReactionActionEvent) -> str:
+    if payload.guild_id is not None:
+        return f"[{(await bot.fetch_guild(payload.guild_id)).name}]"
+    else:
+        return str("[NOT GUILD]")
