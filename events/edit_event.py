84ce4fe94622d23fe2467bbd1ef1a814d8aaaaec
yasharkind <yasharkind95@gmail.com>
2026-08-16 11:55:21 +0330

    chore: i finally installed a linter

--- events/edit_event.py
+++ events/edit_event.py
@@ -25,14 +25,19 @@ class EditEvent(commands.Cog):
         assert payload.guild_id
 
         _config = None
-        try: 
+        try:
             _config = await self.dao.get_guild_cfg(payload.guild_id)
-        except OperationalError as _:
-            self.logger.error("Message edit listenr db error, editcfg table/row might not exist in the database")
+        except OperationalError:
+            self.logger.error(
+                    "Message edit listenr db error, "
+                    "editcfg table/row might not exist in the database"
+                    )
             return
 
         if not _config:
-            self.logger.error(f"Message edit config for {payload.guild_id} not found.")
+            self.logger.error(f"Message edit config for {payload.guild_id}"
+                              "not found."
+                              )
 
         channel = self.bot.get_channel(payload.channel_id)
 
@@ -42,17 +47,30 @@ class EditEvent(commands.Cog):
 
         assert type(message.author) is Member
 
-        if set([role.id for role in message.author.roles]).intersection(_config.ignore_roles):
+        if set(
+                [role.id for role in message.author.roles]
+               ).intersection(_config.ignore_roles):
             return
 
-
-        if (datetime.now(tz=UTC) - message.created_at).total_seconds() * 3600 > _config.time_limit_h:
+        if (
+                datetime.now(
+                    tz=UTC
+                    ) - message.created_at
+                ).total_seconds() * 3600 > _config.time_limit_h:
             report_channel = self.bot.get_channel(_config.report_channel_id)
             assert type(report_channel) is TextChannel
             await report_channel.send(
-                    f"{[f"<@{i}>" for i in _config.mention_list] if _config.mention_list else ""}\n{message.author.name} edited a [message]({message.jump_url}) that was posted <t:{floor(message.created_at.timestamp())}:R>.\n{f"# Before:\n```{payload.cached_message.content}```\n" if payload.cached_message else "\n"}# After:\n```{message.content}```"
+                    f"{
+                        [f"<@{i}>" for i in _config.mention_list]
+                        if _config.mention_list else ""}"
+                    f"\n{message.author.name} edited a "
+                    f"[message]({message.jump_url}) that was posted "
+                    f"<t:{floor(message.created_at.timestamp())}:R>.\n"
+                    f"{
+                        f"# Before:\n```{payload.cached_message.content}```\n"
+                        if payload.cached_message else "\n"}# After:\n```"
+                    f"{message.content}```"
             )
-
 
 
 async def setup(bot: commands.Bot):
